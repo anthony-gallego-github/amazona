c17f13b3cfa9604fef47a5714482ef6017d0468a
Anthony Gallego <a.gallego@live.fr>
2020-10-08 16:58:17 +0200

    Render Producs

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products:[
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image:'/images/d1.jpg',
+            price:60,
+            brand: ' Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image:'/images/d1.jpg',
+            price:50,
+            brand: ' Nike',
+            rating: 4.5,
+            numReviews: 5
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image:'/images/d1.jpg',
+            price:70,
+            brand: ' Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+    ]
+}
