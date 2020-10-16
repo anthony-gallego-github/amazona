290a783aed368b8b18c39342a2ba9e2ddf7082ed
Anthony Gallego <a.gallego@live.fr>
2020-10-16 13:43:30 +0200

    Sign In User

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, require: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true, dropDups: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,46 @@
 import express from 'express';
-import User from '../models/userModel'
+import User from '../models/userModel';
+import { getToken, isAuth } from '../util';
 
 const router = express.Router();
+
+router.post('/signin', async (req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    });
+    if(signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken( signinUser ),
+        });
+    }else {
+        res.status(401).send({ message:'Invalid Email or Password.'});
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    const newUSer = await user.save();
+    if (newUSer){
+        res.send({
+            _id: newUSer.id,
+            name: newUSer.name,
+            email: newUSer.email,
+            isAdmin: newUSer.isAdmin,
+            token: getToken( newUSer ),
+        })
+    } else {
+        res.status(401).send({ message:'Invalid User Data.'});
+    }
+})
 
 router.get("/createadmin", async (req, res) => {
     try {
@@ -11,10 +50,10 @@ router.get("/createadmin", async (req, res) => {
             password: '122112' ,
             isAdmin: true
         });    
-        const newUser =await user.save();
+        const newUser = await user.save();
         res.send(newUser);
     } catch (error) {
-        res.send({ msg: error.message });
+        res.send({ message: error.message });
     }
 });
 
